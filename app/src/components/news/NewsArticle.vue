fabffaee6bc6ee1bad93200a7916973cfae00196
mjtakenon <takenon777@gmail.com>
2023-11-09 02:29:23 +0900

    ニュース欄の作成

--- app/src/components/news/NewsArticle.vue
+++ app/src/components/news/NewsArticle.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="news_article">
-    2023/01/01 テスト
+    <div class="news_date">
+      {{ date.toLocaleDateString('ja-jp', {year: "numeric", month: "2-digit", day: "2-digit"}).replace(/\//g,".") }}
+    </div>
+    <div class="news_contents">
+      {{ contents }}
+    </div>
   </div>
 </template>
 
@@ -11,8 +16,36 @@ export default defineComponent({
   components: {},
   setup() {},
   computed: {},
+  props: {
+    date: {
+      require: true,
+      default: new Date(),
+      type: Date,
+    },
+    contents: {
+      require: true,
+      default: "",
+      type: String,
+    }
+  },
 })
 </script>
 
 <style scoped lang="scss">
+.news_article {
+  @apply my-2;
+  @apply flex;
+  @apply text-sm sm:text-base xl:text-lg;
+}
+
+.news_date {
+  @apply basis-20 xl:basis-24;
+  @apply min-w-[4rem] sm:min-w-[5rem] xl:min-w-[6rem];
+  @apply mx-2;
+  //font-family: 'Roboto Mono', sans-serif;
+}
+
+.news_contents {
+  @apply basis-full;
+}
 </style>
--- app/src/components/news/NewsContainer.vue
+++ app/src/components/news/NewsContainer.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="news_container">
-    <NewsText></NewsText>
-    <NewsArticle></NewsArticle>
+    <NewsText class="news_text"></NewsText>
+    <div v-for="(newsArticle, idx) of newsArticles">
+      <div v-if="idx!==0">
+        <hr class="article_partition"/>
+      </div>
+      <NewsArticle :date="newsArticle.date" :contents="newsArticle.contents" ></NewsArticle>
+    </div>
   </div>
 </template>
 
@@ -12,8 +17,17 @@ import NewsArticle from "./NewsArticle.vue";
 
 export default defineComponent({
   components: {NewsArticle, NewsText},
-  setup() {},
+  setup() {
+  },
   computed: {},
+  data() {
+    return {
+      newsArticles: [
+        { date: new Date("2023/11/14"), contents: "THE VOC@LOiD M@STER53に参加します！新譜「花髪のロラ」頒布予定です。スペースE44でお待ちしていますー！" },
+        { date: new Date("2023/11/13"), contents: "公式サイトを作成しました！" },
+      ]
+    }
+  }
 })
 </script>
 
@@ -28,6 +42,15 @@ export default defineComponent({
   @apply shadow-[0_14px_0_0_#64B6CA] sm:shadow-[14px_14px_0_0_#64B6CA];
   //@apply shadow-[14px_14px_0_0_color:var(color.$mihanada-dark-1)];
   @apply sm:rounded-2xl;
+}
+
+.news_text {
+  @apply pb-2;
+}
+
+.article_partition {
+  @apply border;
+  border-color: color.$text-whitegray;
 }
 
 </style>
