f45fbae8a510ba2a50689e420dac7ae60c232af8
mjtakenon <takenon777@gmail.com>
2023-11-11 02:54:19 +0900

    フッターのリンク実装

--- app/src/components/footer/Footer.vue
+++ app/src/components/footer/Footer.vue
@@ -1,16 +1,71 @@
 <template>
-  <div>footer</div>
+  <footer>
+    <div class="circle_1"></div>
+    <div class="circle_2"></div>
+    <div class="copyright">&copy; 2023 Mihanada </div>
+    <FooterLinks
+      class="links"
+      :links="links"
+    ></FooterLinks>
+  </footer>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
+import FooterLinks from "./FooterLinks.vue";
 
 export default defineComponent({
-  components: {},
+  components: {FooterLinks},
+  data() {
+    return {
+      links: [
+        {icon: "mdi:youtube", url: "https://www.youtube.com/@7EC7D8"},
+        {icon: "simple-icons:niconico", url: "https://sp.nicovideo.jp/my/mylist/57471605"},
+      ]
+    }
+  },
   setup() {},
   computed: {},
 })
 </script>
 
 <style scoped lang="scss">
+@use "../../styles/color";
+
+footer {
+  @apply relative;
+  @apply w-full h-[30vw] max-h-[150px];
+  @apply mt-16;
+  overflow: clip;
+}
+
+.copyright {
+  @apply absolute;
+  @apply p-3 sm:pl-8 sm:pb-4 xl:pl-16;
+  @apply bottom-0;
+  @apply text-xs sm:text-base;
+  color: color.$background;
+}
+
+.circle_1 {
+  @apply absolute;
+  width: calc(30vw + 300px); height: calc(30vw + 300px);
+  left: calc(80% - 200px); top: 0;
+
+  @apply rounded-full;
+  background-color: color.$mihanada-dark-1;
+}
+
+.circle_2 {
+  @apply absolute;
+  width: calc(25vw + 250px); height: calc(25vw + 250px);
+  left: calc(-10% - 80px); top: calc(55% - 2vw);
+  @apply rounded-full;
+  background-color: color.$mihanada-dark-2;
+}
+
+.links {
+  @apply absolute;
+  @apply bottom-4 right-0;
+}
 </style>
---
+++ app/src/components/footer/FooterLinks.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="footer_links">
+    <div v-for="link of links.reverse()">
+      <a :href="link.url"><Icon :class="linkClass(link)" :icon="link.icon"></Icon></a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+import {Link} from "../../scripts/entity/Link.ts";
+
+export default defineComponent({
+  components: {},
+  setup() {},
+  computed: {
+    linkClass() {
+      return (link: Link) => { return {
+        'link': true,
+        'niconico': link.icon === 'simple-icons:niconico',
+      }}
+    }
+  },
+  props: {
+    links: {
+      require: true,
+      default: [],
+      type: Array as PropType<Link[]>
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+@use "../../styles/color";
+
+.footer_links {
+  @apply flex;
+  @apply w-fit;
+  @apply ml-auto py-1 md:py-2 px-4 sm:px-8 md:px-10;
+}
+
+.link {
+  @apply w-8 h-8 sm:w-12 sm:h-12 lg:w-14 lg:h-14;
+  @apply mx-2;
+  color: color.$background;
+}
+
+.niconico {
+  @apply w-7 h-7 sm:w-10 sm:h-10 lg:w-12 lg:h-12;
+}
+
+</style>
