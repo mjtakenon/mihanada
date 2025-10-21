29766dba2c4e2bb69a39dcff9c0ec0811a689e8d
mjtakenon <takenon777@gmail.com>
2025-10-21 21:53:37 +0900

    ニュースの追加とロラの通販ページを追加

--- app/src/components/discography/DiscographyCarousel.vue
+++ app/src/components/discography/DiscographyCarousel.vue
@@ -70,6 +70,7 @@ export default defineComponent({
           no: 4, title: "花髪のロラ", description: "人は花に魅せられる話。", imagePath: jacketLoraImg, eventName: "THE VOC@LOiD M@STER53", links: [
             { icon: "mdi:youtube", url: "https://www.youtube.com/watch?v=qHDPo9wofus" },
             { icon: "simple-icons:niconico", url: "https://www.nicovideo.jp/watch/sm43034832" },
+            { icon: "mdi:cart-arrow-down", url: "https://www.melonbooks.co.jp/detail/detail.php?product_id=2156202" },
           ]
         }, {
           no: 3, title: "夜明け、駅前。", description: "駅前で待つ朝の話。", imagePath: jacketYoakeekimaeImg, eventName: "M3-2022秋", links: [
